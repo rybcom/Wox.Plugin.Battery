490d4135c70b07e263c6bc0313c6fda227a53966
michal rybarik <michal.rybarik@gmail.com>
2022-08-19 18:45:19 +0200

    initial commit

---
+++ Battery.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Windows.Forms;
+
+namespace Wox.Plugin.Battery
+{
+    internal static class Battery
+    {
+
+        public static int BatteryLifePercent
+        {
+            get
+            {
+                PowerStatus pwr = SystemInformation.PowerStatus;
+                return (int)(pwr.BatteryLifePercent * 100);
+            }
+        }
+
+        public static int BatteryFullLifetime
+        {
+            get
+            {
+                PowerStatus pwr = SystemInformation.PowerStatus;
+                return pwr.BatteryFullLifetime;
+            }
+        }
+
+        public static int BatteryLifeRemaining
+        {
+            get
+            {
+                PowerStatus pwr = SystemInformation.PowerStatus;
+                return pwr.BatteryLifeRemaining;
+            }
+        }
+
+        public static string BatteryChargeStatus
+        {
+            get
+            {
+                PowerStatus pwr = SystemInformation.PowerStatus;
+                return pwr.BatteryChargeStatus.ToString();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Collections.Generic;
+
+namespace Wox.Plugin.Battery
+{
+    public class WoxBatteryInformation : IPlugin
+    {
+
+        #region wox overrides
+
+        public void Init(PluginInitContext context)
+        {
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> resultList = new List<Result>();
+
+            AddShowBatteryStatusCommand(resultList);
+
+            return resultList;
+        }
+
+        #endregion
+
+        #region commands
+
+        private string GetIconPathByBatteryPercentage(int percentage)
+        {
+            if (percentage < 5)
+            {
+                return "Images\\battery_min.png";
+            }
+            else if(percentage < 15)
+            {
+                return "Images\\battery_low.png";
+            }
+            else if (percentage < 40)
+            {
+                return "Images\\battery_mid.png";
+            }
+            else if (percentage <75)
+            {
+                return "Images\\battery_ok.png";
+            }
+            else
+            {
+                return "Images\\battery.png";
+            }
+        }
+
+        private void AddShowBatteryStatusCommand(List<Result> resultList)
+        {
+            
+            Result result = new Result
+            {
+                Title = $"Battery : {Battery.BatteryLifePercent} %",
+                SubTitle = $"Status : {Battery.BatteryChargeStatus}, Life remaining : {Battery.BatteryLifeRemaining}",
+                Score = 100,
+                IcoPath = GetIconPathByBatteryPercentage(Battery.BatteryLifePercent),
+                Action = e =>
+                {
+                    return true;
+                }
+            };
+
+            resultList.Add(result);
+        }
+
+        #endregion
+
+    }
+}
